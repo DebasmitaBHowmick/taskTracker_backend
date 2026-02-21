19e75b77f344a4a73907913524c9e4b0d08e23de
Debasmita Bhowmick <dbhowmick035@gmail.com>
2026-02-21 17:59:12 +0530

    backend for tasktracker

---
+++ src/routes/task.js
@@ -0,0 +1,4 @@
+//this is for CRUD
+const router = require("express").Router();
+const prisma = require("../prisma");
+//const auth = require.("../")
---
+++ src/server.js
@@ -0,0 +1,20 @@
+/** 
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", require("./routes/auth"));
+app.use("/tasks", require("./routes/tasks"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running" });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started");
+});**/
